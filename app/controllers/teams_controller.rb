3e3586b775f13c9bc24867fdf274c95b6c7369b6
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-11 14:34:01 +0100

    add create action to teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,5 +4,19 @@ class TeamsController < ApplicationController
   end
 
   def create
+    @team = Team.new(team_params)
+    if @team.save
+      flash[:success] = "#{@team.name} team create"
+      redirect_to root_path
+    else
+      flash[:danger] = 'Team not created'
+      render 'new'
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).premit(:name)
   end
 end
