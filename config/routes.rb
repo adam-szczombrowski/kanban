ad7adc08635a01aed620191f67d519fc795eb8a8
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-11 12:08:26 +0100

    add views and routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'pages#index'
+
+  get '/contact' => 'pages#contact'
+  get '/about' => 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
