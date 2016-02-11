de51018135a23eeb4a3f763e4ec2a8b8f4ab428f
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-11 15:01:28 +0100

    add owner? helper method

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,15 @@
 class TeamsController < ApplicationController
+  include TeamsHelper
+  before_action :authenticate_user!
+
   def new
     @team = Team.new
   end
 
   def create
     @team = Team.new(team_params)
+    current_user.team = @team
+    owner?(@team)
     if @team.save
       flash[:success] = "#{@team.name} team create"
       redirect_to @team
@@ -16,14 +21,17 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
+    owner?(@team)
   end
 
   def edit
     @team = Team.find(params[:id])
+    owner?(@team)
   end
 
   def update
     @team = Team.find(params[:id])
+    owner?(@team)
     if @team.update_attributes(team_params)
       flash[:success] = "Team #{@team.name} edited"
       redirect_to @team
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,8 @@
 module TeamsHelper
+  def owner?(team) #checs if current user belongs to team
+    unless current_user.team == team
+      flash[:danger] = 'This is not your team'
+      redirect_to root_path
+    end
+  end
 end
