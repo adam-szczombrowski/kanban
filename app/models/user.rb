cf8b9f003b9f50147fd71a6062fe391404d822f7
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-11 15:23:04 +0100

    generate task model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :team
+  belongs_to :team
 end
